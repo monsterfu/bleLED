14d3635428ccf5562756e9e056450482f30561b6
monsterfu <seanh@seanHtekiMac-mini.local>
2014-10-08 17:59:34 +0800

    22

---
+++ bleLEDTests/bleLEDTests.m
@@ -0,0 +1,40 @@
+//
+//  bleLEDTests.m
+//  bleLEDTests
+//
+//  Created by Monster on 14-10-2.
+//  Copyright (c) 2014年 Monster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface bleLEDTests : XCTestCase
+
+@end
+
+@implementation bleLEDTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
